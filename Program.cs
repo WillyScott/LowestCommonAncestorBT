f9b5201dd63f0259b2311cb0ca26cc65ece87d0b
William Gromme <wgromme@yahoo.com>
2021-07-11 20:08:24 -0400

    Intial code.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace LowestCommonAncestorBT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lowest Common Ancestor of Binary Search Tree!");
+        }
+
+
+
+    }
+
+    public class Solution
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            return null;
         }
     }
+
+    public class TreeNode
+    {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+  }
+
+
 }
